ff26f6f31d1c22d245ea6990a4866f6c309ee49a
ahmad-mhajna <ahmad.mhmad.rashed777@gmail.com>
2022-06-06 23:06:50 +0300

    some styling

--- src/components/pages/Checkout/Checkout.jsx
+++ src/components/pages/Checkout/Checkout.jsx
@@ -1,22 +1,29 @@
 import Button from "../../Button/Button";
 import CheckoutCard from "./CheckoutCard/CheckoutCard";
-
+import "./Checkout.css";
 function Checkout({ cart, addToCart }) {
   let sum = 0;
   cart.forEach((item) => {
     sum += item.price * item.quantity;
   });
   return (
-    <div>
-      {cart.map((food, i) => {
-        return (
-          <CheckoutCard addToCart={addToCart} cart={cart} food={food} key={i} />
-        );
-      })}
-      <div>
-        {sum}
+    <div className="checkout">
+      <div className="card-container">
+        {cart.map((food, i) => {
+          return (
+            <CheckoutCard
+              addToCart={addToCart}
+              cart={cart}
+              food={food}
+              key={i}
+            />
+          );
+        })}
+      </div>
+      <div className="pay">
+        total :{sum}
         <Button
-          text="pay"
+          text="buy"
           onClick={() => {
             addToCart([]);
           }}
--- src/components/pages/Checkout/CheckoutCard/CheckoutCard.jsx
+++ src/components/pages/Checkout/CheckoutCard/CheckoutCard.jsx
@@ -1,50 +1,56 @@
 import React from "react";
 import Button from "../../../Button/Button";
+import { FaArrowDown, FaArrowUp } from "react-icons/fa";
 import "./CheckoutCard.css";
+
 class CheckoutCard extends React.Component {
   render() {
     return (
-      <div className={`card ${this.props.className || ""}`}>
+      <div className={`CheckoutCard ${this.props.className || ""}`}>
         <img
-          className="card-text"
+          className="CheckoutCard-text"
           src={this.props.food.img}
           alt="img of food"
         />
-        <p className="card-text">{this.props.food.name}</p>
-        <p className="card-text">{this.props.food.price}</p>
-        <Button
-          text="+1"
-          onClick={() => {
-            this.props.addToCart((prevState) => {
-              const stateCopy = [...prevState];
-              const foodCopy = stateCopy.find(
-                (item) => item.id === this.props.food.id
-              );
-              foodCopy.quantity++;
-              return stateCopy;
-            });
-          }}
-        />
-        <p className="card-text">{this.props.food.quantity}</p>
-        <Button
-          text="-1"
-          onClick={() => {
-            this.props.addToCart((prevState) => {
-              if (this.props.food.quantity > 1) {
+        <p className="CheckoutCard-text">{this.props.food.name}</p>
+        <p className="CheckoutCard-text">price : {this.props.food.price}</p>
+        <div className="CheckoutCard-text qty">
+          <Button
+            onClick={() => {
+              this.props.addToCart((prevState) => {
                 const stateCopy = [...prevState];
                 const foodCopy = stateCopy.find(
                   (item) => item.id === this.props.food.id
                 );
-                foodCopy.quantity--;
+                foodCopy.quantity++;
                 return stateCopy;
-              } else {
-                return prevState.filter(
-                  (item) => item.id !== this.props.food.id
-                );
-              }
-            });
-          }}
-        />
+              });
+            }}
+          >
+            <FaArrowUp />
+          </Button>
+          <p className="CheckoutCard-text">{this.props.food.quantity}</p>
+          <Button
+            onClick={() => {
+              this.props.addToCart((prevState) => {
+                if (this.props.food.quantity > 1) {
+                  const stateCopy = [...prevState];
+                  const foodCopy = stateCopy.find(
+                    (item) => item.id === this.props.food.id
+                  );
+                  foodCopy.quantity--;
+                  return stateCopy;
+                } else {
+                  return prevState.filter(
+                    (item) => item.id !== this.props.food.id
+                  );
+                }
+              });
+            }}
+          >
+            <FaArrowDown />
+          </Button>
+        </div>
         <Button
           text="Remove"
           onClick={() => {
